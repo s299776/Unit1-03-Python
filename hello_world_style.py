cf88edc3759d0989d7ac981ab9c228c3535c87bb
Ubuntu <ubuntu@ip-172-31-93-33.ec2.internal>
2021-04-27 17:08:02 +0000

    created the hello world, but now its in the correct python format. Note: it ran with no errors.

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created By: Liam Csiffary
+# Date: April 27, 2021
+# This code prints a sentence to the terminal
+
+
+# defining a function that when called displays "Hello, world!" to the terminal
+def main():
+    print("Hello, world!")
+
+
+# if __name__ == "__main__" then call function "main"
+if __name__ == "__main__":
+    main()
